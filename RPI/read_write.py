59640e18a79aa374a2d6fdea16f1d91aa455a131
jerome <jmusitel@gmail.com>
2023-08-04 18:01:46 +0200

    récupération du niveau d'assistance depuis l'application

--- RPI/read_write.py
+++ RPI/read_write.py
@@ -4,19 +4,25 @@ import time
 uart = UART(0, 57600)
 
 sensor = ADC(Pin(26))
-test=0
+level=0
 boucle=0
 
 while True:
     
     if uart.any() > 0 :
-        test = uart.read()
-        test = str(test)
-        print(test)
-        boucle=boucle+1
-        print(boucle)
+        level = uart.read()
+        level = str(level)
+        level = level[2]
+        if level == "1" :
+            print("Niveau 1")
+        elif level =="2" :
+            print("Niveau 2")
+        elif level == "3" :
+            print("Niveau 3")
+        else :
+            print("error")
+        
     val_sensor = sensor.read_u16()
     simply_val_sensor = round(val_sensor/100)
     uart.write(str(simply_val_sensor))
-    print(simply_val_sensor)
-    time.sleep(5)
+    time.sleep(1)
